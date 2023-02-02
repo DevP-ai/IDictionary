141af2885e4ad8bc1c256ae98aa87a2635a6131b
DevP-ai <patar1447@gmail.com>
2023-02-02 20:51:59 +0530

    Dictionary Api Call Added

---
+++ app/src/main/java/com/dev/android/idictionary/Models/ApiResponse.java
@@ -0,0 +1,36 @@
+package com.dev.android.idictionary.Models;
+
+import org.w3c.dom.ls.LSInput;
+
+import java.util.List;
+
+public class ApiResponse {
+    String word="";
+    List<Phonetics> phonetics=null;
+    List<Meanings> meanings=null;
+
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public List<Phonetics> getPhonetics() {
+        return phonetics;
+    }
+
+    public void setPhonetics(List<Phonetics> phonetics) {
+        this.phonetics = phonetics;
+    }
+
+    public List<Meanings> getMeanings() {
+        return meanings;
+    }
+
+    public void setMeanings(List<Meanings> meanings) {
+        this.meanings = meanings;
+    }
+}
